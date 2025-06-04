96544b3742272b015ec76e15ace69eb3b41bbb63
gana <ganat0460@gmail.com>
2025-06-04 08:00:05 +0300

    change place

--- Assets/Scripts/CarColour.cs
+++ Assets/Scripts/CarColour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarColorChanger : MonoBehaviour
+public class CarColour : MonoBehaviour
 {
     [SerializeField] private MeshRenderer[] targetRenderers;
     private Material[] instancedMaterials;
